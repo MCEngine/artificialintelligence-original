19195f89d8d8407f708a4a434f21fe02e75f4cae
JetsadaWijit <jetsadawijit@outlook.com>
2025-04-12 10:06:10 +0000

    Create MCEngineArtificialIntelligence.java

--- mcengine-artificialintelligence-api/src/main/java/io/github/mcengine/api/artificialintelligence/MCEngineArtificialIntelligence.java
+++ mcengine-artificialintelligence-api/src/main/java/io/github/mcengine/api/artificialintelligence/MCEngineArtificialIntelligence.java
@@ -1,3 +1,28 @@
 package io.github.mcengine.api.artificialintelligence;
 
-public class MCEngineArtificialIntelligence {}
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Logger;
+
+public class MCEngineArtificialIntelligence {
+
+    private final Plugin plugin;
+    private final Logger logger;
+
+    public MCEngineArtificialIntelligence(Plugin plugin) {
+        this.plugin = plugin;
+        this.logger = plugin.getLogger();
+    }
+
+    public void info(String message) {
+        logger.info(message);
+    }
+
+    public void warn(String message) {
+        logger.warning(message);
+    }
+
+    public void error(String message) {
+        logger.severe(message);
+    }
+}
